756161819879452829fe5305b083a157fbba5513
Aslan <aslanmikalaev@gmail.com>
2023-06-05 19:09:10 +0300

    RickAndMorty app created

---
+++ RickAndMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMorty
+//
+//  Created by Аслан Микалаев on 05.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
